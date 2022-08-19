65879a27924aa4dffe4ff62ab07440caf149faed
Barbra Mududa <barbra.mududa@student.moringaschool.com>
2022-08-19 22:04:44 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby'];
+function findMatching(drivers, string) {
+    return drivers.filter((match) => match.toLowerCase() === string.toLowerCase());
+}
+
+function fuzzyMatch(drivers, nameOfDriver){
+    return drivers.filter((match) => match.toLowerCase().indexOf(nameOfDriver.toLowerCase()) === 0);
+}
+
+function matchName(drivers, nameDriver) {
+    return drivers.filter((result) => result.name === nameDriver)
+}
+
